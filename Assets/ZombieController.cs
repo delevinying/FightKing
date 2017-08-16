93df2b2ca0ed2474901a17e0deadda3c0edc355a
delevinying <846353427@qq.com>
2017-08-16 14:11:02 +0800

    3D格斗包含模型

---
+++ Assets/ZombieController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour {
+
+	private Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+		_animator = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.W)) {
+//			_animator.SetInteger ("state", 1);
+			_animator.SetBool ("isAttact", true);
+			_animator.SetBool ("isBack", false);
+			_animator.SetBool ("isLeft", false);
+			_animator.SetBool ("isRight", false);
+		}
+		if (Input.GetKeyDown (KeyCode.S)){
+//			_animator.SetInteger ("state", 3);
+			_animator.SetBool ("isAttact", false);
+			_animator.SetBool ("isBack", true);
+			_animator.SetBool ("isLeft", false);
+			_animator.SetBool ("isRight", false);
+		}
+		if (Input.GetKeyDown (KeyCode.A)){
+//			_animator.SetInteger ("state", 4);
+			_animator.SetBool ("isAttact", false);
+			_animator.SetBool ("isBack", false);
+			_animator.SetBool ("isLeft", true);
+			_animator.SetBool ("isRight", false);
+		}
+		if (Input.GetKeyDown (KeyCode.D)){
+//			_animator.SetInteger ("state", 2);
+			_animator.SetBool ("isAttact", false);
+			_animator.SetBool ("isBack", false);
+			_animator.SetBool ("isLeft", false);
+			_animator.SetBool ("isRight", true);
+		}
+	}
+}

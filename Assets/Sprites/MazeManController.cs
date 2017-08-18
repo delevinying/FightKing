8505f03f331177bee3e00687b38a9b3169ee2b43
delevinying <846353427@qq.com>
2017-08-18 16:50:49 +0800

    新增迷宫吃豆人模块
    吃豆人资源导入

---
+++ Assets/Sprites/MazeManController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeManController : MonoBehaviour {
+
+	// Use this for initialization
+
+	private int speed = 1;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.W)) {
+			transform.Translate (Vector2.right * speed * Time.deltaTime);
+		}
+	}
+}

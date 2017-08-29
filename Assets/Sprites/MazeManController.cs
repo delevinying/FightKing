512730c5bc4c5e81854993b67a3fad1b5905b97b
delevinying <846353427@qq.com>
2017-08-29 14:04:32 +0800

    。。。

--- Assets/Sprites/MazeManController.cs
+++ Assets/Sprites/MazeManController.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class MazeManController : MonoBehaviour {
 
-	// Use this for initialization
+	private int l_rom;
 
 //	private int speed = 1;
 	private GameObject cherryObject;//目标对象
 	void Start () {
 		
 	}
-	
+	private int _temp = 0;
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey(KeyCode.W)) {
@@ -22,6 +22,18 @@ public class MazeManController : MonoBehaviour {
 			gameObject.transform.position += new Vector3 (0.1f, 0, 0);
 		}else if (Input.GetKey(KeyCode.S)) {
 			gameObject.transform.position += new Vector3 (0, -0.1f, 0);
+		}
+		if (_temp == 0) {
+			l_rom = Random.Range (0, 5); 
+			_temp++;
+		}
+		if (_temp > 20) {
+			_temp = 0;
+		}
+		if (l_rom == 1) {
+			transform.position = new Vector3 (0,0.1f,0);
+		} else if(l_rom ==2){
+			transform.position = new Vector3 (0.1f,0,0);
 		}
 	}
 }

4fb935a647e3d08e67d0e48e78e618ea8d6e8ec0
delevinying <846353427@qq.com>
2017-09-04 20:21:48 +0800

    加入随机移动

---
+++ Assets/RandomAi.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAi : MonoBehaviour {
+
+	float stopTime;
+	float moveTime;
+	float vel_x,vel_y,vel_z;
+	float maxPos_x = 55;//500;
+	float maxPos_y = -2;//300;
+	float minPos_x = 56;//-500;
+	float minPos_y = -30;//-300;
+	int curr_frame;
+	int total_frame;
+	float timeCounter1;
+	float timeCounter2;
+	// Use this for initialization
+	void Start () {
+		changePos ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeCounter1 += Time.deltaTime;
+		if (timeCounter1 < moveTime) {
+			transform.Translate (vel_x, vel_y, 0, Space.Self);
+		} else {
+			timeCounter2 += Time.deltaTime;
+			if (timeCounter2 > stopTime) {
+				checkState ();
+				timeCounter1 = 0;
+				timeCounter2 = 0;
+			}
+		}
+		checkState ();
+	}
+
+	void changePos(){
+		stopTime = Random.Range (1, 5);
+		moveTime = Random.Range (1, 20);
+		vel_x = Random.Range (1, 10);
+		vel_y = Random.Range (1, 10);
+	}
+
+	void checkState(){
+		if (transform.localPosition.x > maxPos_x) {
+			vel_x = -vel_x;
+			transform.localPosition = new Vector3 (maxPos_x, transform.localPosition.y, 0);
+		}
+		if (transform.localPosition.x < minPos_x) {
+			vel_x = -vel_x;
+			transform.localPosition = new Vector3 (minPos_x, transform.localPosition.y, 0);
+		}
+		if (transform.localPosition.y > maxPos_y) {
+			vel_y = -vel_y;
+			transform.localPosition = new Vector3 (transform.localPosition.x, maxPos_y, 0);
+		}
+		if (transform.localPosition.y < minPos_y) {
+			vel_y = -vel_y;
+			transform.localPosition = new Vector3 (transform.localPosition.x, minPos_y, 0);
+		}
+	}
+}
--- Assets/Sprites/MazeManController.cs
+++ Assets/Sprites/MazeManController.cs
@@ -4,16 +4,75 @@ using UnityEngine;
 
 public class MazeManController : MonoBehaviour {
 
-	private int l_rom;
-
-//	private int speed = 1;
+	int moveFlag;//移动方向 0静止 1上，2右 3下 4左
 	private GameObject cherryObject;//目标对象
+	int maxStopTime = 2;//最大停留时间
+	float timeCounter1;
+	float timeCounter2;
+	float last_x = -100;
+	float last_y = -100;
+	float dec_x;
+	float dec_y;
+	float stopTimeCounter;
+
 	void Start () {
-		
+		randomMoveState();
 	}
-	private int _temp = 0;
-	// Update is called once per frame
+		
 	void Update () {
+		//controlFun();
+		if (moveFlag == 0) {//静止状态
+			timeCounter1 += Time.deltaTime;
+			if (timeCounter1 > maxStopTime) {//静止时间过久。重新随机运动状态
+				randomMoveState ();
+			}
+		} else {
+			if (timeCounter2 > 5 && checkMoveState ()) {
+				last_x = transform.localPosition.x;
+				last_y = transform.localPosition.y;
+				timeCounter2 = 0;
+				return;
+			}
+			if(moveFlag == 1){
+				gameObject.transform.position += new Vector3 (0, -0.2f, 0);
+			}else if(moveFlag == 2){
+				gameObject.transform.position += new Vector3 (0.2f, 0, 0);
+			}else if(moveFlag == 3){
+				gameObject.transform.position += new Vector3 (0, 0.2f, 0);
+			}else if(moveFlag == 4){
+				gameObject.transform.position += new Vector3 (-0.2f, 0, 0);
+			}
+			timeCounter2 += Time.deltaTime;
+		}
+		last_x = transform.localPosition.x;
+		last_y = transform.localPosition.y;
+
+	}
+
+	bool checkMoveState(){
+		if (last_x != -100 || last_y != -100) {
+			dec_x = Mathf.Abs (transform.localPosition.x - last_x);
+			dec_y = Mathf.Abs (transform.localPosition.y - last_y);
+			Debug.Log ("dec_x   " + dec_x);
+			Debug.Log ("dec_y   " + dec_y);
+			if (dec_x < 0.19 && dec_y < 0.19) {
+//				stopTimeCounter += Time.deltaTime;
+//				if (stopTimeCounter > maxStopTime) {
+					randomMoveState ();
+//				}
+				return true;
+			}
+		}
+		return false;
+	}
+
+	void randomMoveState(){
+		moveFlag = Random.Range (0, 4);
+
+		Debug.Log("moveFlag     "+moveFlag);
+	}
+
+	void controlFun(){
 		if (Input.GetKey(KeyCode.W)) {
 			gameObject.transform.position += new Vector3 (0, 0.1f, 0);
 		}else if (Input.GetKey (KeyCode.A)) {
@@ -22,18 +81,6 @@ public class MazeManController : MonoBehaviour {
 			gameObject.transform.position += new Vector3 (0.1f, 0, 0);
 		}else if (Input.GetKey(KeyCode.S)) {
 			gameObject.transform.position += new Vector3 (0, -0.1f, 0);
-		}
-		if (_temp == 0) {
-			l_rom = Random.Range (0, 5); 
-			_temp++;
-		}
-		if (_temp > 20) {
-			_temp = 0;
-		}
-		if (l_rom == 1) {
-			transform.position = new Vector3 (0,0.1f,0);
-		} else if(l_rom ==2){
-			transform.position = new Vector3 (0.1f,0,0);
 		}
 	}
 }

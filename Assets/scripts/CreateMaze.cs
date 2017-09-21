0ef6fe608610c67cbfaafa6efa1de637b0976a4e
yyc <delevinyyc@gmail.com>
2017-09-21 18:49:50 +0800

    生成墙面

---
+++ Assets/scripts/CreateMaze.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMaze : MonoBehaviour {
+
+	public GameObject wallItem;
+	public int startX;
+	public int startY;
+	public int[,] mazeNum=new int[,]
+	{ 
+		{1,0,1,1,1,1,1,1,1,1},
+		{1,0,0,1,0,0,0,0,0,1},
+		{1,1,0,1,0,1,0,1,0,1},
+		{1,0,0,0,0,1,1,1,0,1},
+		{1,0,1,1,0,0,0,1,0,1},
+		{1,0,0,1,0,1,1,1,0,1},
+		{1,1,0,0,0,1,0,0,0,1},
+		{1,0,0,1,0,1,1,0,1,1},
+		{1,1,0,0,0,0,0,0,0,1},
+		{1,1,1,1,1,1,1,1,0,1}
+	};
+	// Use this for initialization
+	void Start () {
+		drawWall ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void drawWall(){
+		for (int x = 0; x < 10; x++)
+		{
+			for (int y = 0; y < 10; y++)
+			{
+				if (mazeNum [x, y] == 1) {
+					Instantiate (wallItem, new Vector3 (startX + 2*(x - 1), 0, startY + 2*(y - 1)), Quaternion.identity);
+				}
+			}
+		}
+	}
+}
